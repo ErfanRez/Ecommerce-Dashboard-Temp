751e5b8adaabf0b6c3db60f507728ba1459d8793
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-08-13 03:34:22 +0300

    store modal created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
@@ -11,3 +12,32 @@ const SetupPage = () =>  {
 }
 
 export default SetupPage
+=======
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+      Root Page
+    </div>
+  );
+};
+
+export default SetupPage;
+>>>>>>> fbab57d (store modal created)
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+<<<<<<< HEAD
+=======
+import { ModalProvider } from '@/providers/modal-provider'
+>>>>>>> fbab57d (store modal created)
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +22,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
+<<<<<<< HEAD
       <body className={inter.className}>{children}</body>
+=======
+      <body className={inter.className}>
+        <ModalProvider />
+        {children}
+      </body>
+>>>>>>> fbab57d (store modal created)
     </html>
     </ClerkProvider>
   )
